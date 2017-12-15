a7c20cfb3d364993ff083996da94f5d4c3461619
Victor Fugazzotto <victorfuga@gmail.com>
2017-12-15 17:34:57 -0500

    Basic Setup
    Character animator has basic setup and script to test it

---
+++ Assets/KeepBool.cs
@@ -0,0 +1,21 @@
+﻿using UnityEngine;
+
+public class KeepBool : StateMachineBehaviour
+{
+    public string boolName;
+    public bool status;
+    public bool resetOnExit = true;
+
+    // OnStateUpdate is called on each Update frame between OnStateEnter and OnStateExit callbacks
+    override public void OnStateUpdate(Animator animator, AnimatorStateInfo stateInfo, int layerIndex)
+    {
+        animator.SetBool(boolName, status);
+	}
+
+	// OnStateExit is called when a transition ends and the state machine finishes evaluating this state
+    override public void OnStateExit(Animator animator, AnimatorStateInfo stateInfo, int layerIndex)
+    {
+        if (resetOnExit)
+            animator.SetBool(boolName, !status);
+	}
+}
