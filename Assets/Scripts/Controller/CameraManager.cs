48e2e29efc27e72453b1a70faf0564b4005c8d6f
Victor Fugazzotto <victorfuga@gmail.com>
2017-12-15 23:25:10 -0500

    Third Person Controller
    Started third person controller

---
+++ Assets/Scripts/Controller/CameraManager.cs
@@ -0,0 +1,100 @@
+﻿using UnityEngine;
+
+namespace Gazzotto.Controller
+{
+    public class CameraManager : MonoBehaviour
+    {
+        public static CameraManager singleton;
+
+        public bool lockon;
+        public float followSpeed = 3;
+        public float mouseSpeed = 2;
+        public float controllerSpeed = 5;
+
+        public Transform target;
+
+        [HideInInspector] public Transform pivot;
+        [HideInInspector] public Transform camTransform;
+
+        float turnSmoothing = 0.1f;
+        public float minAngle = -35f;
+        public float maxAngle = 35f;
+
+        float smoothX;
+        float smoothY;
+        float smoothXVel;
+        float smoothYVel;
+        public float lookAngle;
+        public float tiltAngle;
+
+        private void Awake()
+        {
+            if (singleton != null)
+                Destroy(gameObject);
+
+            singleton = this;
+        }
+
+        public void Init(Transform t)
+        {
+            target = t;
+
+            camTransform = Camera.main.transform;
+            pivot = camTransform.parent;
+        }
+
+        public void Tick(float d)
+        {
+            float h = Input.GetAxis("Mouse X");
+            float v = Input.GetAxis("Mouse Y");
+
+            float c_h = Input.GetAxis("RightStick X");
+            float c_v = Input.GetAxis("RightStick Y");
+
+            float targetSpeed = mouseSpeed;
+
+            if (c_h != 0 || c_v != 0)
+            {
+                h = c_h;
+                v = c_v;
+                targetSpeed = controllerSpeed;
+            }
+
+            FollowTarget(d);
+            HandleRotations(d, v, h, targetSpeed);
+        }
+
+        void FollowTarget(float d)
+        {
+            float speed = d * followSpeed;
+            Vector3 targetPosition = Vector3.Lerp(transform.position, target.position, speed);
+            transform.position = targetPosition;
+        }
+
+        void HandleRotations(float d, float v, float h, float targetSpeed)
+        {
+            if (turnSmoothing > 0)
+            {
+                smoothX = Mathf.SmoothDamp(smoothX, h, ref smoothXVel, turnSmoothing);
+                smoothY = Mathf.SmoothDamp(smoothY, v, ref smoothYVel, turnSmoothing);
+            }
+            else
+            {
+                smoothX = h;
+                smoothY = v;
+            }
+
+            if (lockon)
+            {
+                // TODO: comeback later
+            }
+
+            lookAngle += smoothX * targetSpeed;
+            transform.rotation = Quaternion.Euler(0, lookAngle, 0);
+
+            tiltAngle -= smoothY * targetSpeed;
+            tiltAngle = Mathf.Clamp(tiltAngle, minAngle, maxAngle);
+            pivot.localRotation = Quaternion.Euler(tiltAngle, 0, 0);
+        }
+    }
+}
---
+++ Assets/Scripts/Controller/InputHandler.cs
@@ -0,0 +1,70 @@
+﻿using UnityEngine;
+
+namespace Gazzotto.Controller
+{
+    public class InputHandler : MonoBehaviour
+    {
+        float vertical;
+        float horizontal;
+        bool runInput;
+
+        StateManager states;
+        CameraManager camManager;
+
+        float delta;
+
+        private void Start()
+        {
+            states = GetComponent<StateManager>();
+            states.Init();
+
+            camManager = CameraManager.singleton;
+            camManager.Init(transform);
+        }
+
+        private void FixedUpdate()
+        {
+            delta = Time.fixedDeltaTime;
+            GetInput();
+            UpdateStates();
+            states.FixedTick(delta);
+            camManager.Tick(delta);
+        }
+
+        private void Update()
+        {
+            delta = Time.deltaTime;
+            states.Tick(delta);
+
+        }
+
+        void GetInput()
+        {
+            vertical = Input.GetAxis("Vertical");
+            horizontal = Input.GetAxis("Horizontal");
+            runInput = Input.GetButton("RunInput");
+        }
+
+        void UpdateStates()
+        {
+            states.horizontal = horizontal;
+            states.vertical = vertical;
+
+            Vector3 v = vertical * camManager.transform.forward;
+            Vector3 h = horizontal * camManager.transform.right;
+            states.moveDir = (v + h).normalized;
+            float m = Mathf.Abs(horizontal) + Mathf.Abs(vertical);
+            states.moveAmount = Mathf.Clamp01(m);
+
+            if (runInput)
+            {
+                if (states.moveAmount > 0)
+                    states.run = true;
+            }
+            else
+            {
+                states.run = false;
+            }
+        }
+    }
+}
